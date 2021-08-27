8702bf5d801f3c4a46760c7aae7fd522c02f126e
Aswathy Saji <aswathy2saji@gmail.com>
2021-08-27 05:43:03 -0700

    Initial Commit

---
+++ events/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('<int:year>/<str:month>/',views.home,name="home"),
+    path('events',views.all_events,name="event_list"),
+    path('add_venue',views.add_venue,name="add-venue"),
+    path('venues',views.venues,name="venues"),
+    path('show_venue/<venue_id>',views.show_venue,name="show-venue"),
+    path('search_venue',views.search_venue,name="search-venue"),
+    path('update_venue/<venue_id>',views.update_venue,name="update-venue"),
+    path('add_event',views.add_event,name="add-event"),
+    path('update_event/<event_id>',views.update_event,name="update-event"),
+    path('delete_event/<event_id>',views.delete_event,name="delete-event"),
+    path('delete_venue/<venue_id>',views.delete_venue,name="delete-venue"),
+    path('venue_text',views.venue_text,name="venue-text"),
+]
---
+++ events/views.py
@@ -0,0 +1,143 @@
+from django.shortcuts import redirect, render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+from django.http import HttpResponseRedirect, response
+from .models import Event, Venue
+from .forms import VenueForm, EventForm
+from django.http import HttpResponse
+
+# Create your views here.
+def venue_text(request):
+    response = HttpResponse(content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename = venues.txt'
+    venues = Venue.objects.all()
+    lines = []
+    for venue in venues:
+        lines.append(f'{venue.name}\n{venue.address}\n{venue.zip_code}\n{venue.phone}\n{venue.web}\n{venue.email_address}\n\n\n')
+    response.writelines(lines)
+    return response
+
+
+def delete_venue(request,venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    venue.delete()
+    return redirect('venues')
+
+def delete_event(request,event_id):
+    event = Event.objects.get(pk=event_id)
+    event.delete()
+    return redirect('event_list')
+
+def update_event(request,event_id):
+    event = Event.objects.get(pk=event_id)
+    form = EventForm(request.POST or None ,instance=event)
+    if form.is_valid():
+        form.save()
+        return redirect('event_list')
+    return render(request,'events/update_event.html',{
+        "event" : event,
+        "form"  : form,
+    })
+
+
+def add_event(request):
+    submitted = False
+    if(request.method == 'POST'):
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_event?submitted=True')
+    else:
+        form = EventForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request,'events/add_event.html',{
+        "form" : form,
+        "submitted" : submitted,
+    })
+
+def update_venue(request,venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    form = VenueForm(request.POST or None ,instance=venue)
+    if form.is_valid():
+        form.save()
+        return redirect('venues')
+    return render(request,'events/update_venue.html',{
+        "venue" : venue,
+        "form"  : form,
+    })
+
+def search_venue(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        venues = Venue.objects.filter(name__contains=searched)
+        return render(request,'events/search_venue.html',{
+            'searched' : searched,
+            "venues" : venues
+            })
+    else:
+        return render(request,'events/search_venue.html',{})
+
+def show_venue(request,venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request,'events/show_venue.html',{
+        "venue" : venue
+    })
+
+def venues(request):
+    venue_list = Venue.objects.all().order_by('name')
+    return render(request,'events/venue.html',{
+        "venue_list" : venue_list
+    })
+
+
+def add_venue(request):
+    submitted = False
+    if(request.method == 'POST'):
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request,'events/add_venue.html',{
+        "form" : form,
+        "submitted" : submitted,
+    })
+
+def all_events(request):
+    event_list = Event.objects.all().order_by('name')
+    return render(request,'events/events_list.html',{
+        "event_list" : event_list
+    })
+
+def home(request,year=datetime.now().year,month=datetime.now().strftime('%B')):
+
+    #convert month to uppercase
+    month = month.capitalize()
+
+    #convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    #create a calendar
+    cal = HTMLCalendar().formatmonth(theyear=year,themonth=month_number)
+
+    #get current year
+    now = datetime.now()
+    current_year = now.year
+
+    #get current time
+    time = now.strftime('%I:%M %p')
+
+    return render(request,'events/home.html',{
+        "year" : year,
+        "month": month,
+        "month_number" : month_number,
+        "cal" : cal,
+        "current_year" : current_year,
+        "time" : time,
+    })
